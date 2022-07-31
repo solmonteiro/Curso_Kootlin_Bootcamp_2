86a27d68d0bbbe11f915ac483e901817ee24d998
solmonteiro <sole7mares@gmail.com>
2022-07-30 22:54:16 -0300

    Initial commit

---
+++ app/src/main/java/com/example/curso_kootlin_bootcamp_2/Practice_Time_Basic_Operations.kt
@@ -0,0 +1,13 @@
+package com.example.curso_kootlin_bootcamp_2
+
+fun main() {
+
+   var peixesRestantes= 2*(71+233)-13
+   val aquariosNecessarios=peixesRestantes/30
+
+   peixesRestantes=2.
+
+   println("Temos $peixesRestantes peixes restantes")
+   println("Necessito de $aquariosNecessarios aquários")
+
+}
